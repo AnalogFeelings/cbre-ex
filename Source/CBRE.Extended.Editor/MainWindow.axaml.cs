0f880a26f742094a80a0ef8ed1028f535e08c95e
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-12-04 00:46:44 +0100

    Add initial Avalonia stuff.

---
+++ Source/CBRE.Extended.Editor/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CBRE.Extended.Editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
