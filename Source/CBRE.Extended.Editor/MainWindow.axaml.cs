48c73e73fbc5c2c021d91673224d839d7fd1f63e
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-12-04 21:10:06 +0100

    Add window title functions for editor.

--- Source/CBRE.Extended.Editor/MainWindow.axaml.cs
+++ Source/CBRE.Extended.Editor/MainWindow.axaml.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Reflection;
 using Avalonia.Controls;
 
 namespace CBRE.Extended.Editor
 {
     public partial class MainWindow : Window
     {
+        private static readonly Version _Version = Assembly.GetEntryAssembly()!.GetName().Version!;
+        private readonly string _TitleStart = $"CBRE-EX v{_Version.ToString(3)}";
+        
         public MainWindow()
         {
             InitializeComponent();
+            UpdateTitle();
+        }
+
+        private void UpdateTitle()
+        {
+            // TODO: Check if document is opened, blah blah.
+            this.Title = _TitleStart + " - No documents opened.";
         }
     }
 }
