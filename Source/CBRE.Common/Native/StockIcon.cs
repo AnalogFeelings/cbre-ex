86c951c444d8f01f4d457c403c5766130252244c
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2023-07-25 02:35:55 +0200

    Remove old crud.

--- Source/CBRE.Common/Native/StockIcon.cs
+++ Source/CBRE.Common/Native/StockIcon.cs
@@ -5,7 +5,7 @@ namespace CBRE.Common.Native
 {
     public class StockIcon
     {
-        [DllImport("Shell32.dll", SetLastError = false)]
-        public static extern Int32 SHGetStockIconInfo(StockIconId iconId, StockIconFlags iconFlags, ref StockIconInfo iconInfo);
+        [DllImport("shell32.dll", SetLastError = false)]
+        public static extern int SHGetStockIconInfo(StockIconId iconId, StockIconFlags iconFlags, ref StockIconInfo iconInfo);
     }
 }
